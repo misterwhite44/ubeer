aedaa76d1a9e72c9360fb6d2176843bbdccd34a3
louisboucard <louis44.boucard@gmail.com>
2025-01-14 14:56:28 +0100

    tt

--- src/breweries.js
+++ src/breweries.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./css/breweries.css";
 
-const BASE_URL = "http://127.0.0.1:5000"; // URL de base de ton API
+const BASE_URL = "http://127.0.0.1:5000"; 
 
 function Breweries() {
-  const [breweries, setBreweries] = useState([]); // Stockage des brasseries
-  const [loading, setLoading] = useState(true); // Indicateur de chargement
-  const [error, setError] = useState(null); // Gestion des erreurs
+  const [breweries, setBreweries] = useState([]); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(null); 
 
-  // Appel API pour récupérer les brasseries
+
   useEffect(() => {
     const fetchBreweries = async () => {
       try {
@@ -16,9 +16,9 @@ function Breweries() {
         if (!response.ok) throw new Error("Erreur lors du chargement des brasseries.");
         
         const data = await response.json();
-        console.log(data); // Vérifie les données retournées par l'API
+        console.log(data); 
 
-        setBreweries(data); // Met à jour la liste des brasseries
+        setBreweries(data); 
       } catch (err) {
         setError(err.message);
       } finally {
@@ -35,7 +35,6 @@ function Breweries() {
       {loading && <p>Chargement en cours...</p>}
       {error && <p className="error">{error}</p>}
 
-      {/* Affichage des brasseries */}
       <div className="brewery-grid">
         {breweries.length > 0 ? (
           breweries.map((brewery) => (
@@ -43,7 +42,6 @@ function Breweries() {
               <h3>{brewery.name}</h3>
               <p>Description : {brewery.description}</p>
               <p>Localisation : {brewery.location}</p>
-              {/* Affichage de l'image */}
               <img
                 src={brewery.image_url ? brewery.image_url : "https://via.placeholder.com/150"}
                 alt={brewery.name}

a0b118781578cbcb00c2d81a3bc6a9bcee15b0d9
louisboucard <louis44.boucard@gmail.com>
2025-01-17 13:47:24 +0100

    ddd

--- src/Auth0Provider.js
+++ src/Auth0Provider.js
@@ -8,7 +8,7 @@ ReactDOM.render(
     domain="dev-bsr5wcm1dymdd5i0.us.auth0.com" // Remplacez par votre domaine Auth0
     clientId="Wabr9OPou6zkfOnaev4e9M7d7tb03P0l" // Remplacez par votre client ID Auth0
     authorizationParams={{
-      redirect_uri: window.location.origin // Redirige vers /home après connexion
+      redirect_uri: `${window.location.origin}/home` // Redirige vers /home après connexion
     }}
   >
     <App />

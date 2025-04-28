2907ef9fc9e5e4c60721a793728a556efff9138a
louisboucard <louis44.boucard@gmail.com>
2025-04-28 10:50:07 +0200

    ajout test

---
+++ tests/home.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Home page - vérifier les éléments principaux', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const heroTitle = await page.getByRole('heading', { level: 2 });
+  await expect(heroTitle).toHaveText('Livraison de bière à domicile en un clic !');
+
+  const orderButton = await page.getByRole('button', { name: 'Commandez Maintenant' });
+  await expect(orderButton).toBeVisible();
+});

298307cf1036a400fb2a3da2117eaaff50b8699c
louisboucard <louis44.boucard@gmail.com>
2025-05-13 17:37:21 +0200

    jvdisd

--- playwright.config.js
+++ playwright.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
-  timeout: 10000,
+  timeout: 20000,
   expect: {
-    timeout: 5000
+    timeout: 20000
   },
   use: {
     baseURL: 'http://localhost:3000',
